71bb49985232195b47f33202178b01bf65e2c812
sandeshofficial092-code <sandeshofficial092@gmail.com>
2026-02-25 13:49:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandeshgod:<Fo1wOMUcCGgRZpTd>@sandesh.patuk22.mongodb.net/")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandeshgod:Fo1wOMUcCGgRZpTd@sandesh.patuk22.mongodb.net/telegrambot?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
