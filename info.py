b22549de88883eeaa225a682e5079249f2b068b5
sandeshofficial092-code <sandeshofficial092@gmail.com>
2026-02-25 11:30:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandeshgod:<Fo1wOMUcCGgRZpTd>@sandesh.patuk22.mongodb.net/")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Sandeshgod")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
