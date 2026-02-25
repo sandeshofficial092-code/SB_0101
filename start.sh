172d33ed502e80c0dd65b5dd818140feb91a1906
sandeshofficial092-code <sandeshofficial092@gmail.com>
2026-02-25 11:31:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandeshofficial092/SB_0101 /SB_0101
+  git clone https://github.com/sandeshofficial092/SB_0101/SB_0101
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SB_0101
